cadaf8505726d19092ac6bfd254ca0752ae44aa1
Vincent Herreros <vincent-herreros@hotmail.fr>
2018-03-13 13:00:18 -0700

    Re-création des vues simples

---
+++ PrescriptionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PrescriptionViewController.swift
+//  Pilalheure
+//
+//  Created by Vincent Herreros on 13/03/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import UIKit
+
+class PrescriptionViewController: UIViewController {
+
+    @IBOutlet weak var prescriptionTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
