a346af898251971cc44b58b588ec73ef3b4473fb
Alexandre.BOTTERO <alex.bottero@hotmail.fr>
2018-03-14 13:57:16 +0100

    motif botbot

---
+++ ExercicePhysiqueTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ExercicePhysiqueTableViewCell.swift
+//  Pilalheure
+//
+//  Created by Bottero Alexandre  on 14/03/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import UIKit
+
+class ExercicePhysiqueTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nomExercicePhysique: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
