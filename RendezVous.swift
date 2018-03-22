dead2a604b4499b5e04927c67094da64226d795c
Alexandre.BOTTERO <alex.bottero@hotmail.fr>
2018-03-22 16:18:24 +0100

    en corus 2

---
+++ RendezVous.swift
@@ -0,0 +1,35 @@
+//
+//  RendezVous.swift
+//  Pilalheure
+//
+//  Created by Bottero Alexandre  on 22/03/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import Foundation
+class RendezVous {
+    
+    
+    internal let dao : RendezVousDTO
+    internal let daoE : EventDTO
+    
+    var date : Date{
+        return self.dao.date! as Date
+    }
+    var contact : ContactDTO{
+        return self.dao.contacts!
+    }
+    
+    
+    init(date: Date, contact: ContactDTO){
+        guard let dao = RendezVousDTO.createDTO() else{
+            fatalError("unuable to get dao for medicament")
+        }
+        guard let daoE = EventDTO.createDTO() else{
+            fatalError("unuable to get dao for medicament")
+        }
+        self.dao = dao
+        self.dao.contacts = contact
+    }
+}
+
