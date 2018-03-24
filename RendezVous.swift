afb89d06174823f8f170e7adde44a40b89aaff5a
Alexandre.BOTTERO <alex.bottero@hotmail.fr>
2018-03-24 15:28:52 +0100

    la c'est mieux

--- RendezVous.swift
+++ RendezVous.swift
@@ -41,31 +41,41 @@ class RendezVous {
         self.dao.date = date as NSDate
         self.dao.heureDebut = heureDeb as NSDate?
         self.dao.heureFin = heureFin as NSDate?
-        var rappels = [Date]()
-        rappels = createRappels(heureRappel: date)
+        var rappelsQuestionnaire = [Date]()
+        let rappelRdv: Date = createRappels(heureRappel: date)
         
-        for i in rappels{
+        if (contact.profession == "neurologue"){
+            rappelsQuestionnaire = createRappels(heureDebut: heureDeb!, heureFin: heureFin!, dateFin: date)
+        }
+        
+        for i in rappelsQuestionnaire{
             guard let daoR = RappelDTO.createDTO() else{
-                fatalError("unuable to get dao for medicament")
+                fatalError("unuable to get dao for Rappel")
             }
             daoR.dateRappel = i as NSDate
             daoR.events = self.daoE
         }
+        guard let daoR = RappelDTO.createDTO() else{
+            fatalError("unuable to get dao for Rappel")
+        }
+        daoR.dateRappel = rappelRdv as NSDate
+        daoR.events = self.daoE
     }
     
     
     
-    func createRappels(heureRappel hR : Date) -> [Date]{
+    func createRappels(heureRappel hR : Date) -> Date{
         
         let heureRappel = Calendar.current.date(byAdding: .hour, value: -1, to: hR)
-        var rappels = [Date]()
-        rappels.append(heureRappel!)
-        return rappels
+        var rappel : Date
+        rappel = heureRappel!
+        return rappel
     }
     
     
-   func createRappels(heureDebut hdeb : Date, heureFin hfin : Date, dateDebut debut : Date, dateFin fin : Date) -> [Date]{
+   func createRappels(heureDebut hdeb : Date, heureFin hfin : Date, dateFin fin : Date) -> [Date]{
         //intervalle de temps entre 2 dates
+        let debut = Calendar.current.date(byAdding: .day, value: -5, to: fin)
         let timeInterval = hfin.timeIntervalSince(hdeb)
         // conversion en Int
         let dif = Int64(timeInterval)
@@ -81,7 +91,7 @@ class RendezVous {
         //change the time
         var date = gregorian.date(from: componentsDD)!
         var dDay = calendar.component(.day, from: date)
-        let dEnd = calendar.component(.day, from: debut)
+        let dEnd = calendar.component(.day, from: debut!)
         while dDay <= dEnd {
             var componentsD = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
             componentsD.hour = componentsHD.hour
