199705001f5e8a107919603d0272b566b429be4b
Vincent Herreros <vincent-herreros@hotmail.fr>
2018-03-27 18:41:00 +0200

    show prescri

--- ShowPrescriptionViewController.swift
+++ ShowPrescriptionViewController.swift
@@ -9,44 +9,66 @@
 import UIKit
 import CoreData
 
-class ShowPrescriptionViewController: UIViewController {
+class ShowPrescriptionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate{
+    
+    @IBOutlet weak var rappelTable: UITableView!
     
     var prescription : PrescriptionDTO? = nil
 
+    /// Récupère tous les médicaments contenu dans le CoreData
+    fileprivate lazy var rappelFetched : NSFetchedResultsController<RappelDTO> = {
+        //préparation de la requete
+        let request : NSFetchRequest<RappelDTO> = RappelDTO.fetchRequest()
+        //Ajout d'un tri sur la requete : trie pat les noms de médicaments
+        request.sortDescriptors = [NSSortDescriptor(key:#keyPath(RappelDTO.dateRappel), ascending:true)]
+        let predicate = NSPredicate(format:"events.prescriptions.medicaments.nom = %@", (self.prescription?.medicaments?.nom)!)
+        request.predicate = predicate
+        let fetchResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: CoreDataManager.context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchResultController.delegate = self
+        return fetchResultController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Background.color(controleur: self)
         // Do any additional setup after loading the view.
-        if let prescri = self.prescription{
-            print("hello")
-            if let date = prescri.events?.rappels{
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "dd-MM-yyyy 'at' HH:mm"
-                for d in date{
-                    if let da = d as? RappelDTO{
-                        let stringDate = dateFormatter.string(from: da.dateRappel as! Date)
-                        print(stringDate)
-                    }
-                }
-            }
+        
+        do{
+            try self.rappelFetched.performFetch()
         }
+        catch let error as NSError{
+            DialogBoxHelper.alert(view: self, error: error)
+        }
+        Background.color(controleur: self)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Table View Data Source protocol -
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = self.rappelTable.dequeueReusableCell(withIdentifier: "rappelCell", for: indexPath) as! ShowPrescriptionTableViewCell
+        let rappel = self.rappelFetched.object(at: indexPath)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        let stringDate = dateFormatter.string(from: rappel.dateRappel! as Date)
+        cell.dateLabel.text = stringDate
+        return cell
+        
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        guard let section = self.rappelFetched.sections?[section] else{
+            fatalError("enexpected section number")
+        }
+        return section.numberOfObjects
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool{
+        return true
+    }
 
+    
 }
